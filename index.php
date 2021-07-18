f861b62d4cfee7f1febab776d5def906f58c5c9a
Mamta149-tech <85868844+Mamta149-tech@users.noreply.github.com>
2021-07-18 18:44:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Cerebro Kids</title>
+        <title>Sign Up</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -145,7 +145,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             <div class="container-fluid">
                 <img class="ms-5" src="https://www.cerebrokids.com/ckimages/nikko.png" alt="" width="60" height="60" class="d-inline-block align-text-top">
                 <a class="text-decoration-none navtitle" href="#">
-                    <span class="fs-2 fw-bold">CerebroKids</span><br><span>The Financial Literacy Academy</span>
+                    <span class="fs-2 fw-bold">Sign Up</span><br><span>Registration</span>
                 </a>
             </div>
         </nav>
